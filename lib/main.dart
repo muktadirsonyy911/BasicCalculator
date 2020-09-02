1088a06fedcc64f343e40bb7e2a6ef03e364d49a
muktadirsonyy911 <47278808+muktadirsonyy911@users.noreply.github.com>
2020-09-02 17:08:30 +0600

    Added Appbar...

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(BasicCalculator());
 }
+
 class BasicCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            "Basic Calculator",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                fontFamily: 'Times New Roman'),
+          ),
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
-
-
